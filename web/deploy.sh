e89caf75cdd7a48a96a6e46dc87604c035ee090f
hjm79.top <me@hjm79.top>
2026-02-01 09:21:22 +0900

    feat: Auto-update sitemap lastmod date on deploy

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # vesslo.top 배포 스크립트
 
+# sitemap.xml 날짜 오늘 날짜로 갱신 (SEO 최적화)
+TODAY=$(date +%Y-%m-%d)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS
+  sed -i '' "s/<lastmod>[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}<\/lastmod>/<lastmod>$TODAY<\/lastmod>/g" public/sitemap.xml
+else
+  # Linux
+  sed -i "s/<lastmod>[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}<\/lastmod>/<lastmod>$TODAY<\/lastmod>/g" public/sitemap.xml
+fi
+echo "📅 Updated sitemap.xml lastmod to $TODAY"
+
 echo "🔨 Building..."
 npm run build
 
